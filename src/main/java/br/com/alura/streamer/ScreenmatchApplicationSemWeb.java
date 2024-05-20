884b4225ae0d85589a96f65997608fa905914aac
Luis Cortêz Dell <112598652+ocorteiz@users.noreply.github.com>
2024-05-20 11:35:05 -0300

    att screenmatchapplication

---
+++ src/main/java/br/com/alura/streamer/ScreenmatchApplicationSemWeb.java
@@ -0,0 +1,28 @@
+/*
+
+package br.com.alura.streamer;
+
+import br.com.alura.streamer.principal.Principal;
+import br.com.alura.streamer.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplicationSemWeb implements CommandLineRunner {
+
+	@Autowired
+	private SerieRepository serieRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplicationSemWeb.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(serieRepository);
+		principal.exibeMenu();
+	}
+}
+*/
